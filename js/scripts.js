8cb6c64ce31b0d1db07149f1f5d4b8852f0f6f1c
Kenna Warsinske & Bojana Skarich <student@epicodus.com>
2015-04-15 14:08:13 -0700

    Add withdraw function in order to be able to withdraw money.

--- js/scripts.js
+++ js/scripts.js
@@ -24,22 +24,34 @@ $(document).ready(function() {
     $("form#new-bankaccount").submit(function(event) {
         event.preventDefault();
         var inputtedName = $("input#new-name").val();
-        var initialDeposit = parseFloat($("input#new-deposit").val()).toFixed(2);
+        var initialDeposit = parseFloat($("input#new-deposit").val());
 
         var createAccount = Object.create(BankAccount);
 
         createAccount.name = inputtedName;
         createAccount.balance = initialDeposit;
-        debugger;
-    });
+
+        //display monies
+        var result = createAccount.balance;
+        $("span.balance").text(result);
+
+        $("form#withdrawlDeposit").submit(function(event) {
+            //includes an event.preventDefault after EVERY submit event
+            event.preventDefault();
+
+            var withdrawlMoney = parseInt($("input#withdrawl").val());
+            var newBalance = createAccount.withdraw(withdrawlMoney);
 
 
-    $("form#withdrawlDeposit").submit(function(event) {
-
-        var withdrawl = parseInt($("input#withdrawl").val());
-
-        var deposit = parseInt($("input#deposit").val());
-    });
+            //display monies
+            //var result = createAccount.balance;
+            $("span.balance").text(createAccount.balance);
 
 
-});
+            //var depositMoney = parseInt($("input#deposit").val());
+
+        });//Ends form#withdrawlDeposit
+
+    });//Ends form#new-bankaccount
+
+});//Ends document.ready
