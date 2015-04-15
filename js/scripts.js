03a074c4dcd6d56e0d38855808c1ddf28383c3a1
Kenna Warsinske & Bojana Skarich <student@epicodus.com>
2015-04-15 11:28:51 -0700

    add name and initial deposit form with submit

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,37 @@
 var BankAccount = {
+    name = null;
     balance: 0,
 
     deposit: function(amount) {
-        this.balance = this.balance +amount;
+        this.balance = this.balance + amount;
     },
 
     withdraw:function(amount) {
         this.balance = this.balance - amount;
     }
 };
+
+var newAccount = Object.create(BankAccount);
+    newAccount.balance;
+    newAccount.deposit(2500);
+    newAccount.balance;
+
+
+
+$(document).ready(function() {
+
+
+    $("form#new-bankaccount").submit(function(event) {
+        var inputtedName = $("input#new-name").val();
+        var initialDeposit = parseFloat($("input#new-deposit"));
+    });
+
+
+    $("form#withdrawlDeposit").submit(function(event) {
+
+        var withdrawl = parseInt($("input#withdrawl").val());
+
+        var deposit = parseInt($("input#deposit").val());
+    });
+
+});
