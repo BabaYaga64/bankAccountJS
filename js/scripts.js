fa43e5c064d93ceed3d3db9d0bd21fd6d7d797fe
Kenna Warsinske & Bojana Skarich <student@epicodus.com>
2015-04-15 11:59:03 -0700

    complete create new account

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 var BankAccount = {
-    name = null;
+    name: null,
     balance: 0,
 
     deposit: function(amount) {
@@ -11,19 +11,26 @@ var BankAccount = {
     }
 };
 
-var newAccount = Object.create(BankAccount);
-    newAccount.balance;
-    newAccount.deposit(2500);
-    newAccount.balance;
+// var newAccount = Object.create(BankAccount);
+//     newAccount.balance;
+//     newAccount.deposit(2500);
+//     newAccount.balance;
 
 
 
 $(document).ready(function() {
 
-
+    //On the click submit button, pass the name form input into a variable inputtedName. Also, grab the integer from the new deposit input and set it to a number with two places to right of decimal.
     $("form#new-bankaccount").submit(function(event) {
+        event.preventDefault();
         var inputtedName = $("input#new-name").val();
-        var initialDeposit = parseFloat($("input#new-deposit"));
+        var initialDeposit = parseFloat($("input#new-deposit").val()).toFixed(2);
+
+        var createAccount = Object.create(BankAccount);
+
+        createAccount.name = inputtedName;
+        createAccount.balance = initialDeposit;
+        debugger;
     });
 
 
@@ -33,5 +40,6 @@ $(document).ready(function() {
 
         var deposit = parseInt($("input#deposit").val());
     });
+
 
 });
